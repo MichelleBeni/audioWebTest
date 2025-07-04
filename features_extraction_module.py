ac2d31f16b6c18b36b996c20b4948da1c2f38943
MichelleBeni <35224688+MichelleBeni@users.noreply.github.com>
2025-07-04 20:14:58 +0300

    Update features_extraction_module.py

--- features_extraction_module.py
+++ features_extraction_module.py
@@ -5,7 +5,7 @@ def extract_extra_features(file_path):
     y, sr = librosa.load(file_path)
     pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
     pitch_values = librosa.yin(y, fmin=50, fmax=500, sr=sr)
-    pitch_var = pitch_values.std()
+    pitch_var = np.std(pitch_values)
     pitch_diff = abs(pitch_values[1:] - pitch_values[:-1])
     pitch_rate = pitch_diff.mean()
     return pitch_var, pitch_rate
