eaa93697f9185902c0230587576e1fa284ffc440
MichelleBeni <35224688+MichelleBeni@users.noreply.github.com>
2025-07-05 08:18:16 +0300

    Update features_extraction_module.py

--- features_extraction_module.py
+++ features_extraction_module.py
@@ -6,7 +6,7 @@ import numpy as np
 def extract_extra_features(filepath):
     y, sr = librosa.load(filepath)
     pitch_values = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'))[0]
-    pitch_values = pitch_values[~np.isnan(pitch_values)]  # חשוב! מסנן ערכים חסרים
+   # pitch_values = pitch_values[~np.isnan(pitch_values)]  # חשוב! מסנן ערכים חסרים
 
     pitch_var = np.std(pitch_values)
 
